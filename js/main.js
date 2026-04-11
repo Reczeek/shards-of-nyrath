93b57856fc67d404f0aeb4f7ea7d7e293bc8a6fd
Reczeek <kreczek25@gmail.com>
2026-04-11 13:44:54 +0200

    Class selection screen working

--- js/main.js
+++ js/main.js
@@ -4,5 +4,36 @@ function showScreen(screenId) {
 }
 
 document.getElementById('btn-newgame').addEventListener('click', () => {
+    showScreen('screen-class');
+    renderClasses();
+});
+
+function renderClasses() {
+    const list = document.getElementById('class-list');
+    list.innerHTML = '';
+    CLASSES.forEach(cls => {
+        const card = document.createElement('div');
+        card.classList.add('class-card');
+        if (!cls.unlocked) card.classList.add('locked');
+        card.innerHTML = `
+            <div class="class-icon">${cls.icon}</div>
+            <div class="class-name">${cls.name}</div>
+            <div class="class-desc">${cls.unlocked ? cls.description : '???'}</div>
+        `;
+        if (cls.unlocked) {
+            card.addEventListener('click', () => {
+                selectClass(cls);
+            });
+        }
+        list.appendChild(card);
+    })
+}
+
+function selectClass(cls) {
+    console.log('Selected class:', cls.name);
     showScreen('screen-game');
+}
+
+document.getElementById('btn-back-menu').addEventListener('click', () => {
+    showScreen('screen-menu');
 });
